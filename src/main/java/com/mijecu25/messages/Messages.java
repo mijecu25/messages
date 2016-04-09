9f00668eddd433883f6b8c584fbd6ff18b80caf6
Miguel Velez <miguelvelez@mijecu25.com>
2016-04-09 12:01:48 -0500

    Fix bug that made this program only work with SQLPlus

--- src/main/java/com/mijecu25/messages/Messages.java
+++ src/main/java/com/mijecu25/messages/Messages.java
@@ -4,7 +4,7 @@ package com.mijecu25.messages;
  * Messages class with debugging messages.
  * 
  * @author Miguel Velez - miguelvelezmj25
- * @version 1.0.0.1
+ * @version 1.0.1.1
  */
 public abstract class Messages {
     
@@ -13,7 +13,6 @@ public abstract class Messages {
     public static final String ERROR = "ERROR: ";
     public static final String FATAL = "FATAL: ";
     public static final String CHECK_LOG_FILES = "Please check the log files.";
-    public static final String QUIT_ERROR = "Quitting SQLPlus because of an error";
     
     /**
      * Used when catching a fatal exception.
@@ -21,17 +20,28 @@ public abstract class Messages {
      * @param exception the exception that was caught
      * @return string with message
      */
-    public static final String FATAL_EXCEPTION_ACTION(String exception) {
+    public static String FATAL_EXCEPTION_ACTION(String exception) {
         return "There was a " + exception + " when your last action was executed.";
     }
-    
+
     /**
-     * Used when exciting because of a fatal exception.
+     * Used when exiting a program because of a fatal exception.
      * 
+     * @param program the name of the program
      * @param exception the exception that was caught
      * @return string with message
      */
-    public static final String FATAL_EXIT(String exception) {
-        return "Exiting SQLPlus because of a " + exception;
+    public static String FATAL_EXIT(String program, String exception) {
+        return "Exiting " + program + " because of a " + exception;
+    }
+    
+    /**
+     * Used when quitting a program because of an error.
+     * 
+     * @param program the name of the program
+     * @return string with message
+     */
+    public static String QUIT_PROGRAM_ERROR(String program) {
+        return "Quitting " + program + " because of an error";
     }
 }
